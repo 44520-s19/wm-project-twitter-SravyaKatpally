47362b04b50c605b931d62e159771fbc0649a0f3
katpallysravya@gmail.com <password@99>
2019-06-19 23:35:52 -0500

    committing all changes

---
+++ nltk_helpers.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#print("Sravya Katpally")
+
+import nltk
+
+
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+ 
+def get_sentiments(text):
+   
+    analyzer= SentimentIntensityAnalyzer()
+    return analyzer.polarity_scores(text)
+
+
+def split_sentiments(sentiments):
+    xs = [sent['neg'] for sent in sentiments]
+    ys = [sent['neu'] for sent in sentiments]
+    zs = [sent['pos'] for sent in sentiments]
+    
+    return xs,ys,zs
+    
